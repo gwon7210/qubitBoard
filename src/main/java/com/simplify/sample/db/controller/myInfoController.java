690d43b8686259092bcaec8c44b1672d1095689e
gwon7210 <gwon7210@naver.com>
2023-12-22 13:58:28 +0900

    d

--- src/main/java/com/simplify/sample/db/controller/myInfoController.java
+++ src/main/java/com/simplify/sample/db/controller/myInfoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class myInfoController {
-
+//d
     @GetMapping("/makingMindMap")
     public String makingMindMap() {
         return ("/makingMindMap");
